f5d9d7ccbd9a0348ff0d86252c0f4ce531520380
MichalKowalski876 <111149364+MichalKowalski876@users.noreply.github.com>
2026-08-07 13:24:00 +0200

    Update reciever.ino

--- reciever.ino
+++ reciever.ino
@@ -7,7 +7,15 @@ DynamicJsonDocument incoming(2048);
 #define TFT_DC   9
 #define TFT_RST  8
 
+#define GRAY tft.color565(28, 29, 41)
+#define GREEN tft.color565(25, 87, 36)
+#define YELLOW tft.color565(138, 145, 32)
+#define RED tft.color565(148, 40, 40)
+#define RED_TEXT tft.color565(255, 79, 79)
+
+
 Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST);
+
 
 void display(float cpu, float ram, float gpu, String ram_per, String gpu_per, float cpu_t, float gpu_t){
   display_clear();
@@ -15,22 +23,31 @@ void display(float cpu, float ram, float gpu, String ram_per, String gpu_per, fl
   tft.drawLine(0, 40, 160, 40, ST77XX_WHITE);
   tft.drawLine(0, 90, 160, 90, ST77XX_WHITE);
   tft.drawLine(60, 140, 60, 0, ST77XX_WHITE);
+  
+  //cpu
+  tft.drawRect(65, 5, 90, 30, ST77XX_WHITE);
+  tft.fillRect(66, 6,  (int)(89 * cpu / 100.0 - 1), 28, color_change(cpu));
+  //gpu
+  tft.drawRect(65, 51, 90, 30, ST77XX_WHITE);
+  tft.fillRect(66, 52, (int)(89 * gpu / 100.0 - 1), 28, color_change(gpu));
+  //ram
+  tft.drawRect(65, 95, 90, 30, ST77XX_WHITE);
+  tft.fillRect(66, 96, (int)(89 * ram / 100.0 - 1), 28, color_change(ram));
 
-  tft.fillRect(65, 5, 90 *(cpu/100), 30, ST77XX_GREEN);
-  tft.fillRect(65, 48, 90 *(gpu/100), 30, ST77XX_GREEN);
-  tft.fillRect(65, 95, 90 *(ram/100), 30, ST77XX_GREEN);
-
+  //cpu
   tft.setCursor(5, 5);
   tft.print("CPU");
   tft.setCursor(5, 15);
   tft.print(cpu, 1);
   tft.print("%");
   tft.setCursor(5, 25);
+  tft.setTextColor(text_color(cpu_t, "c"));
   tft.print(cpu_t, 1);
   tft.print("\xF7");
   tft.print("C");
+  tft.setTextColor(ST77XX_WHITE);
 
-
+  //gpu
   tft.setCursor(5, 48);
   tft.print("GPU");
   tft.setCursor(5, 58);
@@ -39,10 +56,13 @@ void display(float cpu, float ram, float gpu, String ram_per, String gpu_per, fl
   tft.setCursor(5, 68);
   tft.print(gpu_per);
   tft.setCursor(5, 78);
+  tft.setTextColor(text_color(gpu_t, "g"));
   tft.print(gpu_t, 1);
   tft.print("\xF7");
   tft.print("C");
+  tft.setTextColor(ST77XX_WHITE);
 
+//ram
   tft.setCursor(5, 95);
   tft.print("RAM");
   tft.setCursor(5, 105);
@@ -55,14 +75,14 @@ void display(float cpu, float ram, float gpu, String ram_per, String gpu_per, fl
 
 void display_clear(){
   //CPU
-  tft.fillRect(5, 15, 55, 25, ST77XX_BLACK);
-  tft.fillRect(65,5,90,30, ST77XX_BLACK);
+  tft.fillRect(5, 15, 55, 25, GRAY);
+  tft.fillRect(65,5,90,30, GRAY);
   //GPU
-  tft.fillRect(5,58, 55, 30, ST77XX_BLACK);
-  tft.fillRect(65, 48, 90, 30, ST77XX_BLACK);
+  tft.fillRect(5,58, 55, 30, GRAY);
+  tft.fillRect(65, 48, 90, 40, GRAY);
   //RAM
-  tft.fillRect(5, 105, 55, 25, ST77XX_BLACK);
-  tft.fillRect(65, 95, 90, 30, ST77XX_BLACK);
+  tft.fillRect(5, 105, 55, 25, GRAY);
+  tft.fillRect(65, 95, 90, 30, GRAY);
 }
 
 String data_change(float used, float total){
@@ -79,6 +99,31 @@ String data_change(float used, float total){
   return output;
 }
 
+uint16_t color_change(float usage) {
+    if (usage < 60)
+        return GREEN;
+    else if (usage < 80)
+        return YELLOW;
+    else
+        return RED;
+}
+
+uint16_t text_color(float temp, char p) {
+    if (p == "c"){
+      if (temp < 90){
+        return ST77XX_WHITE;
+      } else{
+        return RED_TEXT;
+      }
+    } else{
+      if (temp < 80){
+        return ST77XX_WHITE;
+      } else{
+        return RED_TEXT;
+      }
+    }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(2000);
@@ -86,7 +131,7 @@ void setup() {
   Serial.println("Initializing ST7735 display...");
 
   tft.initR(INITR_BLACKTAB); 
-  tft.fillScreen(ST77XX_BLACK);
+  tft.fillScreen(GRAY);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(1);
   tft.setRotation(1);
@@ -133,3 +178,15 @@ void loop() {
   display(c, r, g, ram_per, gpu_per, tc, tg);
   
 }
+
+
+/*
+GND -> GND
+VCC -> 3.3V
+SCK -> 13
+SDA -> 11
+RST -> 8
+DC  -> 9
+CS  -> 6
+BL  -> 3
+*/
